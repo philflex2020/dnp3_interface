b08346d5b1c830f913ff075d121ff52d366d5873
Phil Wilshire <sysdcs@gmail.com>
2020-05-07 09:21:05 -0400

    value

--- master_test/src/newSOEHandler.cpp
+++ master_test/src/newSOEHandler.cpp
@@ -45,6 +45,7 @@ void newSOEHandler::Process(const HeaderInfo& /* info*/, const ICollection<Index
     auto print = [](const Indexed<Analog>& pair) {
         std::cout << "Analog "
                   << " index [" << pair.index <<"]"
+                  << " value [" << pair.value.value <<"]"
                   << std::endl;
     };
     values.ForeachItem(print);
