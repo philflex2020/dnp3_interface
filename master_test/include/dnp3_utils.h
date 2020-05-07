321e1b27179dcedd1f98d1ec35501a57196a70eb
Phil Wilshire <sysdcs@gmail.com>
2020-05-07 07:35:16 -0400

    maps

--- master_test/include/dnp3_utils.h
+++ master_test/include/dnp3_utils.h
@@ -159,7 +159,7 @@ typedef struct sdata
 
     int setValue(int idx, bool value, int send) 
     {
-        map* mp = regs_to_map[BinaryIP];
+        maps* mp = regs_to_map[BinaryIP];
         if (idx < mp->num_regs)
         {
             mapr * mr = mp->regs[idx];
