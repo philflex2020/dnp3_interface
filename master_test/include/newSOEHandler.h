4765d36f20bf2bc6a3d5560765e36c96141b352b
phil wilshire <pwilshire@flexgen.com>
2020-05-08 09:48:03 -0400

    try this

--- master_test/include/newSOEHandler.h
+++ master_test/include/newSOEHandler.h
@@ -36,12 +36,12 @@ namespace asiodnp3
 class newSOEHandler final : public opendnp3::ISOEHandler 
 { 
 public:
-    newSOEHandler(sysCfg* db) { cfgdb = db;};
+    newSOEHandler(sysCfg* db) { /*cfgdb = db;*/};
     static std::shared_ptr<ISOEHandler> Create(sysCfg* db)
     {
         return std::make_shared<newSOEHandler>(db);
     }
-    void setDB(sysCfg* db){cfgdb = db;};
+    //void setDB(sysCfg* db){cfgdb = db;};
     virtual void Process(const opendnp3::HeaderInfo& info,
                          const opendnp3::ICollection<opendnp3::Indexed<opendnp3::Binary>>& values) override;
     virtual void Process(const opendnp3::HeaderInfo& info,
