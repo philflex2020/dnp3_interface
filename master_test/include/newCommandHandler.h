24d9152354162be6df93842ac40d532df030ad14
phil wilshire <pwilshire@flexgen.com>
2020-05-13 13:23:24 -0400

    build

--- master_test/include/newCommandHandler.h
+++ master_test/include/newCommandHandler.h
@@ -34,16 +34,16 @@ public:
     opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt16& command, uint16_t index, opendnp3::OperateType opType) override;
     //{ return opendnp3::CommandStatus::NOT_SUPPORTED; }
 
-    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt32& command, uint16_t index) override
+    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt32& command, uint16_t index) override;
     //{ return opendnp3::CommandStatus::NOT_SUPPORTED; }
 
-    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt32& command, uint16_t index, opendnp3::OperateType opType) override
+    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt32& command, uint16_t index, opendnp3::OperateType opType) override;
     //{ return opendnp3::CommandStatus::NOT_SUPPORTED; }
 
-    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputFloat32& command, uint16_t index) override
+    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputFloat32& command, uint16_t index) override;
     //{ return opendnp3::CommandStatus::NOT_SUPPORTED; }
 
-    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputFloat32& command, uint16_t index, opendnp3::OperateType opType)
+    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputFloat32& command, uint16_t index, opendnp3::OperateType opType) override;
     //{ return opendnp3::CommandStatus::NOT_SUPPORTED; }
 
     opendnp3::CommandStatus Select(const opendnp3::AnalogOutputDouble64& command, uint16_t index) override
