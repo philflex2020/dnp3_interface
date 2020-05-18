aecb2dd5b1f0e350d23ee9c3649cf07a1675e91b
phil wilshire <pwilshire@flexgen.com>
2020-05-17 21:39:44 -0400

    Update dnp3_master.cpp

--- master_test/src/dnp3_master.cpp
+++ master_test/src/dnp3_master.cpp
@@ -268,7 +268,7 @@ std::shared_ptr<IMaster> setupDNP3master (std::shared_ptr<IChannel> channel, con
     // do an integrity poll (Class 3/2/1/0) once per minute
     //auto integrityScan = master->AddClassScan(ClassField::AllClasses(), TimeDuration::Minutes(1));
     // TODO we need a way to demand this via FIMS
-    auto integrityScan = master->AddClassScan(ClassField::AllClasses(), TimeDuration::Seconds(20);
+    auto integrityScan = master->AddClassScan(ClassField::AllClasses(), TimeDuration::Seconds(20));
     // do a Class 1 exception poll every 5 seconds
     auto exceptionScan = master->AddClassScan(ClassField(ClassField::CLASS_1), TimeDuration::Seconds(60));
     
