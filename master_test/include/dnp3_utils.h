557f3f06b6921f2384bd54fb0c093ef5fe82f742
phil wilshire <pwilshire@flexgen.com>
2020-05-22 13:45:46 -0400

    b

--- master_test/include/dnp3_utils.h
+++ master_test/include/dnp3_utils.h
@@ -574,7 +574,7 @@ typedef struct sysCfg_t {
                 {
                     DbVar* db = it->second[i];
                     char replyto[1024];
-                    snprintf(replyto,sizeof(replyto),"/interfaces/%s%s/reply/%s", id, it->first, db->name.c_str() )
+                    snprintf(replyto,sizeof(replyto),"/interfaces/%s%s/reply/%s", id, it->first, db->name.c_str() );
                     char getUri[1024];
                     snprintf(getUri,sizeof(getUri),"/query%s/%s", it->first, db->name.c_str() )
 
