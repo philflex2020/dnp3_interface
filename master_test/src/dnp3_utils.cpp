e1f24a05bf06b7488ec9e7e8f77298dd61d7a1ad
phil wilshire <pwilshire@flexgen.com>
2020-05-28 11:46:38 -0400

    Update dnp3_utils.cpp

--- master_test/src/dnp3_utils.cpp
+++ master_test/src/dnp3_utils.cpp
@@ -823,7 +823,7 @@ cJSON* parseBody(dbs_type& dbs, sysCfg*sys, fims_message*msg, const char* who)
     uri = msg->pfrags[fragptr];
     if (strncmp(uri, sys->id, strlen(sys->id)) == 0)
     {
-        FPS_ERROR_PRINT("fims message msg->uri [%s] frag 1 [%s] Not DEVEL ACCEPTED  [%s] \n", msg->uri, uri, sys->id;
+        FPS_ERROR_PRINT("fims message msg->uri [%s] frag 1 [%s] Not DEVEL ACCEPTED  [%s] \n", msg->uri, uri, sys->id);
         fragptr = 0;
     }
     else if (strncmp(uri, who, strlen(who)) != 0)
