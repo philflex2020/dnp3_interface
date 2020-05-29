55e4894f847b3e64adf5fb5816b82d872cffa55e
phil wilshire <pwilshire@flexgen.com>
2020-05-29 07:50:31 -0400

    b2

--- master_test/src/dnp3_utils.cpp
+++ master_test/src/dnp3_utils.cpp
@@ -1116,7 +1116,7 @@ void sendCmdSet(sysCfg* sysdb, DbVar*db, cJSON* cj)
 
     if (db->uri)
     {
-        uri = db->uri
+        uri = db->uri;
     }
     else
     {
@@ -1144,7 +1144,7 @@ void sendCmdSet(sysCfg* sysdb, DbVar*db, cJSON* cj)
 // possibly used in outstation comand handler to publish changes
 void sysdbAddtoRecord(sysCfg* sysdb, const char* field, const opendnp3::AnalogOutputInt16& cmd, uint16_t index)
 {
-    DbVar* db = sysdb->getDbVarId(AnIn16 , index)
+    DbVar* db = sysdb->getDbVarId(AnIn16 , index);
     if (db)
     {
         cJSON* cjf = sysdbFindAddArray(sysdb, field);
@@ -1161,7 +1161,7 @@ void sysdbAddtoRecord(sysCfg* sysdb, const char* field, const opendnp3::AnalogOu
 
 void sysdbAddtoRecord(sysCfg* sysdb,const char* field, const opendnp3::AnalogOutputInt32& cmd, uint16_t index)
 {
-    DbVar* db = sysdb->getDbVarId(AnIn32 , index)
+    DbVar* db = sysdb->getDbVarId(AnIn32 , index);
     if (db)
     {
         cJSON* cjf = sysdbFindAddArray(sysdb, field);
@@ -1178,7 +1178,7 @@ void sysdbAddtoRecord(sysCfg* sysdb,const char* field, const opendnp3::AnalogOut
 
 void sysdbAddtoRecord(sysCfg* sysdb,const char* field, const opendnp3::AnalogOutputFloat32& cmd, uint16_t index)
 {
-    DbVar* db = sysdb->getDbVarId(AnF32 , index)
+    DbVar* db = sysdb->getDbVarId(AnF32 , index);
     if (db)
     {
         cJSON* cjf = sysdbFindAddArray(sysdb, field);
@@ -1194,7 +1194,7 @@ void sysdbAddtoRecord(sysCfg* sysdb,const char* field, const opendnp3::AnalogOut
 
 void sysdbAddtoRecord(sysCfg* sysdb, const char* field, const char* cmd, uint16_t index)
 {
-    DbVar* db = sysdb->getDbVarId(AnF32 , index)
+    DbVar* db = sysdb->getDbVarId(AnF32 , index);
     if (db)
     {
         cJSON* cjf = sysdbFindAddArray(sysdb, field);
