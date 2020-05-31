9ccdd5581040153a129abde56ff04174bd404546
phil wilshire <pwilshire@flexgen.com>
2020-05-30 22:04:32 -0400

    Update fpsSOEHandler.cpp

--- master_test/src/fpsSOEHandler.cpp
+++ master_test/src/fpsSOEHandler.cpp
@@ -101,9 +101,18 @@ void fpsSOEHandler::Process(const HeaderInfo& info, const ICollection<Indexed<Bi
     return PrintAll(info, values);
 }
 void fpsSOEHandler::Process(const HeaderInfo& info, const ICollection<Indexed<AnalogOutputStatus>>& values) {
-    FPS_DEBUG_PRINT("******************************AnOpSta: \n");
-    return PrintAll(info, values);
+    FPS_DEBUG_PRINT("******************************AnOpSta: \n"); auto print = [](const Indexed<BinaryCommandEvent>& pair) {
+    std::cout << "AnalogOutputStatus: "
+     auto print = [](const Indexed<AnalogOutputStatus>& pair) {
+                  << "[" << pair.index << "] : ["  << pair.value.value << " ] "
+                  //<< CommandStatusToString(pair.value.status) 
+                  << std::endl;
+    };
+    values.ForeachItem(print);
 }
+
+//     return PrintAll(info, values);
+// }
 void fpsSOEHandler::Process(const HeaderInfo& /*info*/, const ICollection<Indexed<OctetString>>& values) {
     auto print = [](const Indexed<OctetString>& pair) {
         std::cout << "OctetString "
