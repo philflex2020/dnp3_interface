15962db54d4f29a311fecdddc4a4a39a115a661c
philflex2020 <pwilshire@flexgen.com>
2020-06-06 19:58:57 -0400

    nosign now working

--- master_test/scripts/test_AnOPInt32_nosign.sh
+++ master_test/scripts/test_AnOPInt32_nosign.sh
@@ -6,31 +6,31 @@ VAR=remote_manual_solar_kW_cmd
 echo ' normal +1 -1'
 
 VAL=1
-fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":$VAL}"
+fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":$VAL}"
 #{"remote_export_target_kW_cmd":{"value":1}}
 fims_send -m get -u /interfaces/hybridos/$VAR -r /me 
-#fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":-$VAL}"
+fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":-$VAL}"
 #{"remote_export_target_kW_cmd":{"value":-1}}
 #fims_send -m get -u /components/hybridos_m/$VAR -r /me 
 
 echo 'try +/- 65535'
 VAL=65535
 
-fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":$VAL}"
+fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":$VAL}"
 #{"remote_export_target_kW_cmd":{"value":65535}}
 fims_send -m get -u /interfaces/hybridos/$VAR-r /me 
 
-#fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":-$VAL}"
+#fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":-$VAL}"
 #{"remote_export_target_kW_cmd":{"value":-65535}}
-#fims_send -m get -u /interfaces/hybridos/$VAR -r /me 
+fims_send -m get -u /interfaces/hybridos/$VAR -r /me 
 
 #move out to 2147483647
 echo 'try +/- 214748347'
 VAL=2147483647
 
-fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":$VAL}"
+fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":$VAL}"
 #{"remote_export_target_kW_cmd":{"value":214748647}}
-fims_send -m get -u /components/hybridos/$VAR -r /me 
+fims_send -m get -u /interfaces/hybridos/$VAR -r /me 
 #{"remote_export_target_kW_cmd":{"value":214748647}}
 
 #fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":-$VAL}"
@@ -42,7 +42,7 @@ fims_send -m get -u /components/hybridos/$VAR -r /me
 echo ' try +/- 3147483647'
 VAL=3147483647
 
-fims_send -m set -u /components/hybridos/$VAR -r /me "{\"value\":$VAL}"
+fims_send -m set -u /components/hybridos_m/$VAR -r /me "{\"value\":$VAL}"
 #{"remote_export_target_kW_cmd":{"value":214748647}}
 fims_send -m get -u /interfaces/hybridos/$VAR -r /me 
 ##{"remote_export_target_kW_cmd":{"value":214748647}}
