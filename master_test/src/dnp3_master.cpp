13cb5b58e8710edc82e187f6eef1c0f8f1331005
phil wilshire <pwilshire@flexgen.com>
2020-06-06 10:31:53 -0400

    t

--- master_test/src/dnp3_master.cpp
+++ master_test/src/dnp3_master.cpp
@@ -183,10 +183,10 @@ void addVarToCommands (CommandSet & commands, std::pair<DbVar*,int>dbp)
         }
         case AnIn32:
         {
-            int32_t ival = getInt32Val(db);
+            //int32_t ival = getInt32Val(db);
             
-            //commands.Add<AnalogOutputInt32>({WithIndex(AnalogOutputInt32(db->anInt32), db->idx)});
-            commands.Add<AnalogOutputInt32>({WithIndex(AnalogOutputInt32(ival, db->idx)});
+            commands.Add<AnalogOutputInt32>({WithIndex(AnalogOutputInt32(db->anInt32), db->idx)});
+            //commands.Add<AnalogOutputInt32>({WithIndex(AnalogOutputInt32(ival, db->idx)});
             break;
         }
         case AnF32:
